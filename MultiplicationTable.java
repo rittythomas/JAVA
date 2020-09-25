3653d07a501ae0eb2aded6fa11144c7de50d2fa0
Greshma Shaji <43352861+ceccs17d24@users.noreply.github.com>
2020-09-25 17:28:04 +0530

    Multiplication table

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -5,7 +5,7 @@ class MultiplicationTable
   public static void main(String args[])
   {
     int n, c;
-    System.out.println("Enter an integer to print it's multiplication table");
+    System.out.println("Enter an integer value to print it's multiplication table");
     Scanner in = new Scanner(System.in);
     n = in.nextInt();
     System.out.println("Multiplication table of " + n);
